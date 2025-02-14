b61d41167b7f07c58289669de26b1018d4edc574
Jeena01 <jeenajavahar@gmail.com>
2025-02-14 09:53:08 -0800

    set up launch indicator

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -4,7 +4,7 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
     [SerializeField] private Transform ballAnchor;
-
+    [SerializeField] private Transform launchIndicator;
 
 
     [SerializeField] private InputManager inputManager;
@@ -31,6 +31,7 @@ public class BallController : MonoBehaviour
         isBallLaunched = true;
         transform.parent = null;
         ballRB.isKinematic = false;
-        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+        ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
+        launchIndicator.gameObject.SetActive(false);
     }
 }
